9c3c2f3288b3e32713b8c2f4121e07af4c89f806
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-10 20:05:25 +0100

    created project

---
+++ giocoCarte/src/main/java/com/example/giocoCarte/GiocoCarteApplication.java
@@ -0,0 +1,13 @@
+package com.example.giocoCarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiocoCarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiocoCarteApplication.class, args);
+	}
+
+}
---
+++ giocoCarte/src/test/java/com/example/giocoCarte/GiocoCarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giocoCarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiocoCarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
